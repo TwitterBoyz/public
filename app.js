2fb3240b9a7c3a00e7f5a696e57c02708277ce10
Adam Taitano <adam.taitano@gmail.com>
2015-09-14 11:06:00 -0700

    added a html directory

--- app.js
+++ app.js
@@ -3,35 +3,34 @@ var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
 var users = [];
-// var html_dir = './html/';
-
-// app.use(express.static(path.join(__dirname, 'html')));
+var passwords = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/',function(req,res){
-  res.sendfile("index.html");
+  res.sendfile("html/index.html");
 });
 
 app.get('/user',function(req,res){
-  res.sendfile("user.html");
+  res.sendfile("html/user.html");
 });
 
 app.get('/tweet',function(req,res){
-  res.sendfile("tweet.html");
+  res.sendfile("html/tweet.html");
 });
 
 app.get('/main',function(req,res){
-  res.sendfile("main.html");
+  res.sendfile("html/main.html");
 });
 
 app.post('/login',function(req,res){
   var user_name=req.body.user;
   var password=req.body.password;
   users.push(user_name);
+  passwords.push(password);
   console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
-  console.log(users);
+  console.log(users, passwords);
 });
 
 app.listen(3000,function(){
